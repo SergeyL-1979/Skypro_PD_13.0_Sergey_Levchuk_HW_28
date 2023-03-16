63fba4d04eee3ead270266fe5ee64b7c55712e3c
sergd <videomc1979@gmail.com>
2023-03-16 18:37:13 +0300

    HW_28 added new urls

--- ads/urls.py
+++ ads/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path
 from ads import views
-from ads.views import AnnouncementsView, AnnouncementsDetailView, CategoryView, CategoryDetailView
+from .views import (
+    AnnouncementListView, AnnouncementsDetailView,
+    AnnouncementsCreateView, AnnouncementsUpdateView, LocationListView, CategoryCreateView,
+    CategoryListView, CategoryUpdateView, CategoryDetailView, UserListView)
 
 urlpatterns = [
-    path('', views.status),
-    path('ad/', AnnouncementsView.as_view(), name='announce_list'),
-    path('ad/<int:pk>', AnnouncementsDetailView.as_view(), name='announce_detail'),
-    path('cat/', CategoryView.as_view(), name='category_list'),
-    path('cat/<int:pk>', CategoryDetailView.as_view(), name='category_detail'),
+    path('', views.root),
+    path('ad/', AnnouncementListView.as_view(), name='announce_list'),
+    path('ad/<int:pk>/', AnnouncementsDetailView.as_view(), name='announce_detail'),
+    path('ad/update/<int:pk>/', AnnouncementsUpdateView.as_view(), name='announce_update'),
+    path('create/', AnnouncementsCreateView.as_view(), name='create_announce'),
+
+    path('cat/', CategoryListView.as_view(), name='category_list'),
+    path('cat/create/', CategoryCreateView.as_view(), name='category_create'),
+    path('cat/<int:pk>/', CategoryDetailView.as_view(), name='category_detail'),
+    path('cat/update/<int:pk>/', CategoryUpdateView.as_view(), name='category_update'),
+
+    path('location/', LocationListView.as_view(), name='location_list'),
+
+    path('users/', UserListView.as_view(), name='users_list'),
 ]
