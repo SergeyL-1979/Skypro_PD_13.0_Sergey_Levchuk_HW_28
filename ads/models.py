d9c8e3b441a332ba206eed8d99057d60f8459d67
sergd <videomc1979@gmail.com>
2023-03-16 18:39:42 +0300

    HW_28 finished model with M2M and FK links

--- ads/models.py
+++ ads/models.py
@@ -15,6 +15,30 @@ class Category(models.Model):
         return '{}'.format(self.name)
 
 
+class User(models.Model):
+    """ Модель пользователя и автора объявлений """
+    STATUS = [
+        ("admin", "Администратор"),
+        ("moderator", "Модератор"),
+        ("member", "Участник"),
+    ]
+    first_name = models.CharField(_("Имя"), max_length=50)
+    last_name = models.CharField(_("Фамилия"), max_length=50, null=True, blank=True)
+    username = models.CharField(_("Никнейм"), max_length=50)
+    password = models.CharField(_("password"), max_length=150)
+    role = models.CharField(_("Права пользователя"), max_length=10, choices=STATUS, default="member")
+    age = models.IntegerField(_("Возраст"))
+    location = models.ManyToManyField("Location", blank=True, verbose_name='Местоположение')
+    # location = models.ForeignKey(Location, on_delete=models.CASCADE, verbose_name='Местоположение')
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        # ordering = ["username"]
+
+    def __str__(self):
+        return "{} {}".format(self.last_name, (self.username, ))
+
 class Location(models.Model):
     """ Модель местоположения """
     name = models.CharField(_("Местоположение"), max_length=50)
@@ -29,31 +53,6 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class User(models.Model):
-    """ Модель пользователя и автора объявлений """
-    STATUS = [
-        ("admin", "Администратор"),
-        ("moderator", "Модератор"),
-        ("member", "Участник"),
-    ]
-    first_name = models.CharField(_("Имя"), max_length=50)
-    last_name = models.CharField(_("Фамилия"), max_length=50, null=True, blank=True)
-    username = models.CharField(_("Никнейм"), max_length=50)
-    password = models.CharField(_("password"), max_length=150)
-    role = models.CharField(_("Права пользователя"), max_length=10, choices=STATUS, default="member")
-    age = models.IntegerField(_("Возраст"))
-    location = models.ManyToManyField(Location, verbose_name='Местоположение')
-    # location = models.ForeignKey(Location, on_delete=models.CASCADE, verbose_name='Местоположение')
-
-    class Meta:
-        verbose_name = "Пользователь"
-        verbose_name_plural = "Пользователи"
-        ordering = ["username"]
-
-    def __str__(self):
-        return "{} {}".format(self.last_name, (self.username, ))
 
 
 class Announcement(models.Model):
